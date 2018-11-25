bdd67dfe16a37662d2ca65bba05e6a0f4d43b617
Akshath Jain <akshath.r.jain@gmail.com>
2018-11-25 15:47:02 -0500

    added barebones skeleton for code

---
+++ SpectralClustering.py
@@ -0,0 +1,13 @@
+#Name: Akshath Jain
+#Date: 11/25/18
+#Purpose: spectral clustering algorithm for 21241 final project
+
+#main function
+def main():
+	#get user input
+	#construct graph
+	#create adjacency matrix
+	#create laplacian matrix
+	#do PCA (principal component analysis)
+	#do k means clustering
+	#return/print result
